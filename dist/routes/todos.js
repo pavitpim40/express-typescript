8e7aa18477a4a0a126855882517d520250cb9601
pavitpim40 <pavitpim@gmail.com>
2022-05-03 01:01:40 +0700

    middleware Type

---
+++ dist/routes/todos.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = require("express");
+var router = (0, express_1.Router)();
+router.post("/");
+router.get("/");
+router.patch("/");
+router.delete("/:id");
+exports.default = router;
---
+++ src/routes/todos.ts
@@ -0,0 +1,15 @@
+import {Router} from 'express'
+
+
+
+const router = Router()
+
+router.post("/");
+
+router.get("/");
+
+router.patch("/");
+
+router.delete("/:id");
+
+export default router;
